4ac7f847e366e40125ec9297e7148c263eff1f09
Nathachai Thongniran <inid3a@gmail.com>
2023-08-27 20:39:31 +0700

    build(release): release 1.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="cyclical",
-    version="1.0.1",
+    version="1.0.2",
     description="Encode item list into cyclical",
     long_description=README,
     long_description_content_type="text/markdown",
